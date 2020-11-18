d16e604f2cdfc062e382eb54aa31cec1f8b98cbf
Michelle Lee <michellechulee@gmail.com>
2020-11-18 00:44:23 -0800

    login logic

--- js/Recipe.js
+++
@@ -1,7 +0,0 @@
-class Recipe {
-    constructor(name, image, description) {
-        this.name = name;
-        this.image = image;
-        this.description = description;
-    }
-}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+
+
 let name = 'Chocolate Chip Cookies';
 let image = 'cookies.jpg';
 let description = 'Cookies cookies cookies are delicious.';
 
-let cookie = new Recipe(name, image, description);
+let loginPages = ['about.html', 'contact.html', 'gallery.html', 'index.html', 'profile.html', 'recipe.html'];
+let loginTrue = false;
+
+let loginButton = document.getElementById('login');
+loginButton.addEventListener('click', login);
+
+function login() {
+    location.href = 'index.html';
+}
+
+
+

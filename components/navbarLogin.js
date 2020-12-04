451085ba95ee6064b9db53d3794178d8b4bbeff8
Michelle Lee <michellechulee@gmail.com>
2020-12-03 22:04:22 -0800

    logo + 100% search bar

--- components/navbarLogin.js
+++ components/navbarLogin.js
@@ -1,20 +1,17 @@
 const navbar = `
     <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="index.html"><img src="img/logo50x50.png"></a>
-
+        <div class="container"><a class="navbar-brand" href="index.html">Cooking Cards</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="browse.html">Browse</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.html">About</a>
+                        <a class="nav-link" href="about.html">About Us</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="contact.html">Contact</a>
@@ -22,11 +19,9 @@ const navbar = `
                 </ul>
                 <div class="search-bar">
                     <i class="fas fa-search"></i>
-                    <input id="search" type="text" placeholder="Search recipes"></div>
+                    <input id="search" type="text" placeholder="Search for cooking recipes"></div>
                 </div>
-                <div id="login-authentication">
-                    <a href="login.html"><button type="button" class="btn btn-outline-primary">Login/Register</button></a>
-                </div>
+                <a href="profile.html"></a><img src="img/profile.png" id="profile-pic"></a>
             </div>
         </div>
     </nav>`;
--- js/login.js
+++ js/login.js
@@ -1,38 +1,8 @@
-// get login button 
-let loginBtn = document.getElementById('login');
-let loginAuthBtn = document.querySelector('#login-authentication a');
-let loginAuthWrapper = document.getElementById('login-authentication');
+const loginBtn = document.getElementById('login');
 
-{/* <div id="login-authentication">
-    <a href="profile.html"><img src="img/profile.png" class="profile-icon" alt="profile"></a>
-</div> */}
-
-/**
- * click on login, make login/register disappear for all pages, add profile
- * icon for all pages
- * 
- * show heart icons only if login is authenticated
- */
-// loginBtn.addEventListener('click', function() {
-//     loginAuthBtn.classList.add('toggle-off'); //toggle the login button
-//     loginAuthWrapper.classList.add('authenticated'); //innhtml don't work cause dynamic, use innerHTML to create literal DOM?
-//     let element = createProfileIcon();
-//     let navbar = document.getElementById('login-authentication');
-//     navbar.appendChild(element);
-//     // window.location.href = 'index.html';
-// });
-
-// function createProfileIcon() {
-//     let a = document.createElement('a');
-//     a.setAttribute('href', 'profile.html');
-//     let img = document.createElement('img');
-//     img.setAttribute('src', 'img/profile.png');
-//     img.setAttribute('class', 'profile-icon');
-//     img.setAttribute('alt', 'profile');
-    
-//     let navbar = document.getElementById('login-authentication'); //must attach to existing element
-//     let element = a.appendChild(img);
-//     element = navbar.appendChild(a);
-//     // console.log(element)
-//     return element;
-// }
+let login = false;
+if(!login) {
+    // print navbar without profile icon
+} else {
+    // print navbar with profile icon
+}
